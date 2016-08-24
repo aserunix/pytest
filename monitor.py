f74eab3426eec722bde06538c268eec435e65c6d
Administrator <aserunix@gmail.com>
2016-08-24 14:12:30 +0800

    monitor test

---
+++ monitor.py
@@ -0,0 +1,9 @@
+UsageCpu={'us':0,'us':0,'ni':0,'id':0,'wa':0,'hi':0,'si':0,'st':0}
+UsageMem={'total':0,'used':0,'free':0,'shard':0,'buffers':0,'cached':0,'real_used':0,'real_free':0}
+UsageSwap={'total':0,'used':0,'free':0,'shard':0,'buffers':0,'cached':0,'real_used':0,'real_free':0}
+UsageDiskSpace={}
+UsageDiskInode={}
+UsageDiskIO={}
+UsageNicIOPackets={}
+UsageNicIOBytes={}
+UsageNicSpeedRate={}
