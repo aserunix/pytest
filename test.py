fbc59429f21f55d56c162370c2f2460ecd8d4c06
aser <aserunix@gmail.com>
2016-11-23 22:37:50 +0800

    test

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
+
+import os
+try:
+    data=open('missing.txt','w')
+    # print(data.readline(),end='')
+    print("It's...",file=data)
+except IOError as err:
+    print('File error: ' + str(err) )
+finally:
+    if 'data' in locals():
+        data.close()
