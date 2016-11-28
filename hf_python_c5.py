148a660061b72d8773527f06de8284d7ee608e61
aser <aserunix@gmail.com>
2016-11-28 22:11:04 +0800

    test

--- hf_python_c5.py
+++ hf_python_c5.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
+
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter='-'
+    elif ':' in time_string:
+        splitter=':'
+    else:
+        return(time_string)
+    (mins,secs)=time_string.split(splitter)
+    return(mins+'.'+secs)
+
+with open('julie.txt') as juf:
+    data=juf.readline()
+julie=data.strip().split(',')
+
+clean_julie=[]
+for each_time in julie:
+    clean_julie=clean_julie.append(sanitize(each_time))
+# print(sorted(clean_julie))
+# print(clean_julie)
+
+# a=[6,3,2,5,4]
+# a=sorted(a)
+# print(a)
