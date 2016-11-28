d5e023ea821c7ff13a0675a21932d0a0e0208a7a
aserunix <aserunix@gmail.com>
2016-11-28 21:30:18 +0800

    header

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 #/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+import os
+os.chdir('/home/jiangshijian/PycharmProjects/pytest')
+
+with open('julie.txt') as  juf:
+    data=juf.readline()
+    julie=data.strip().split(',')
+    print(juf)
