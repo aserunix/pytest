5804ad411e4e831e7c348b54d85576285351cfa3
aser <aserunix@gmail.com>
2016-11-29 00:01:35 +0800

    test

--- hf_py_c6.py
+++ hf_py_c6.py
@@ -2,4 +2,10 @@
 #-*- coding:utf-8 -*-
 
 class Athlete:
-    def
+    def __int__(self,value=0):
+        ##the code to initialize a "Athlete" object
+        self.thing=value
+    def how_big(self):
+        return(len(self.thing))
+
+d=Athlete("Holy Grail")
